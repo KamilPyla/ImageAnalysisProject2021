66beba66baaee3a46abcaa1a1cc3f9644c95dee6
Kamil Pyla <kamilpyla@gmail.com>
2022-01-20 19:37:34 +0100

    Delete unnecessary files

--- Searcher.py
+++
@@ -1,2 +0,0 @@
-# Input: Converted Image
-# Output: Number Plates (String) and Number Plates Location (on the given converted image)
--- Tests.py
+++
@@ -1,6 +0,0 @@
-import Reader
-import Converter
-
-class Test:
-    def __init__(self, image_path):
-        self.image_path = image_path
