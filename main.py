5337c684c6a1c444ef9e5d4211e2bd77d9351030
Raadwal <76631231+Raadwal@users.noreply.github.com>
2022-01-20 19:00:19 +0100

    Converter + Reader + Doc

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from Converter import Converter
 from Reader import Reader
 from cv2 import cv2
 
+'''
 if __name__ == "__main__":
     EXAMPLES_NUMBER = 9
     EXAMPLES_PATH = "example-img/"
     path_list = []
     image_list = []
-    extensions = ["jpg", "jpeg"]
-    '''
+    extensions = ["jpg", "jpeg", "png"]
+
     for i in range(EXAMPLES_NUMBER):
         path_list.append(EXAMPLES_PATH + "Sample-" + str(i + 1) + ".jpg")
 
@@ -26,24 +27,4 @@ if __name__ == "__main__":
         converter = Converter(image_list[i])
         converter.convert_image()
         converter.show_copy()
-   '''
-
-    path = EXAMPLES_PATH + "Sample-4.jpg"
-    path_test = EXAMPLES_PATH + "Sample-4-test.jpg"
-
-    reader = Reader(extensions)
-
-    reader.set_path(path)
-    original = Converter(reader.get_image())
-
-    reader.set_path(path_test)
-    test = Converter(reader.get_image())
-
-    original.show_original()
-    test.show_original()
-
-    original.convert_image()
-    test.convert_image()
-
-    original.show_copy()
-    test.show_copy()
+'''
